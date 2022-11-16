6ae995a4b21ea73d150775130ab08333dfe7a240
Tayyab Ali <tayyabofficial78@gmail.com>
2022-11-17 01:44:19 +0500

    All Files uplaod!

---
+++ PDF Viewer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PDF_Viewer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PDF Viewer/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace PDF_Viewer;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void UrlToPdf(object sender, EventArgs e)
+	{
+		if (URL.Text != null)
+		{
+            var Renderer = new IronPdf.ChromePdfRenderer();
+            var pdf = Renderer.RenderUrlAsPdf(URL.Text.Trim());
+            SaveService saveService = new();
+            saveService.SaveAndView("URLtoPDF.pdf", "application/pdf", pdf.Stream);
+            DisplayAlert("Success", "PDF from URL Created!", "OK");
+        }
+		else
+		{
+            DisplayAlert("Error", "Field can't be empty! \nPlease enter URL!", "OK");
+        }
+
+    }
+
+    private void HtmlToPdf(object sender, EventArgs e)
+    {
+        if (HTML.Text != null)
+        {
+            var Renderer = new IronPdf.ChromePdfRenderer();
+            var pdf = Renderer.RenderHtmlAsPdf(HTML.Text);
+            SaveService saveService = new();
+            saveService.SaveAndView("IronPDF HTML string.pdf", "application/pdf", pdf.Stream);
+            DisplayAlert("Success", "PDF from HTML Created!", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "Field can't be empty! \nPlease enter valid HTML!", "OK");
+        }
+    }
+
+    private void FileToPdf(object sender, EventArgs e)
+    {
+        var Renderer = new IronPdf.ChromePdfRenderer();
+        var pdf = Renderer.RenderHtmlFileAsPdf(@"C:\Users\Administrator\Desktop\index.html");
+        SaveService saveService = new();
+        saveService.SaveAndView("HTML FIle to PDF.pdf", "application/pdf", pdf.Stream);
+        DisplayAlert("Success", "PDF from File Created!", "OK");
+    }
+}
+
